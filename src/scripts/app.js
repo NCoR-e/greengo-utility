d843e9d2edb8988f80ccd5884040cc1d4587a203
Dmitriy <codingncrploy@gmail.com>
2026-08-31 01:23:42 +0300

    refactor: Client-side data preparation for future features, client-side data storage in localStorage (temporary-test implementation) - Updating the card rendering for future features to be added to the UI - Adding/modifying attributes in the markup - A script has been written to implement a new method for rendering cards - A script was written to save data on the client side in localStorage (temporarily) - The scripting code paradigm is modeled after functional programming (possibly temporarily)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,87 @@
 import '@styles/index.scss';
+let rootElement = document.querySelector('[data-root]');
+let rootSection = rootElement.querySelector('[data-card-list]');
+let modalList = rootElement.querySelectorAll('dialog[data-modal]');
+let activeState = null;
+
+const INITIAL_STATE = [
+    {
+        id: crypto.randomUUID(), type: 'main',
+        countryCode: 'ch', countryName: 'Швейцария',
+        panelLink: '#',
+    },
+
+    {
+        id: crypto.randomUUID(), type: 'main',
+        countryCode: 'se', countryName: 'Швеция',
+        panelLink: '#',
+    },
+
+    {
+        id: crypto.randomUUID(), type: 'main',
+        countryCode: 'us', countryName: 'США',
+        panelLink: '#',
+    },
+
+    {
+        id: crypto.randomUUID(), type: 'test',
+        countryCode: 'ch', countryName: 'Швейцария',
+        panelLink: '#',
+    },
+];
+
+function getInitialState(storageKey, fallbackState) {
+    let savedState = localStorage.getItem(storageKey);
+    return savedState ? JSON.parse(savedState) : fallbackState;
+}
+
+function fabricCard(data, rootEl) {
+    let templateCard = rootEl.querySelector(`template[data-template-card="${data.type}"]`);
+    if (!templateCard) return null;
+
+    let cardElement = templateCard.content.cloneNode(true);
+    let rootElementCard = cardElement.querySelector('[data-card-type]');
+
+    let titleCard = rootElementCard.querySelector('[data-info="title"]');
+    let iconCard = rootElementCard.querySelector('[data-info="icon"]');
+    let x3PanelCard = rootElementCard.querySelector('[data-button="redirect-panel"]');
+
+    if (titleCard && iconCard && x3PanelCard) {
+        titleCard.textContent = `${data.countryCode.toUpperCase()} ${data.countryName}`;
+        iconCard.setAttribute('href', `/sprites.svg#flag-${data.countryCode}`);
+        x3PanelCard.setAttribute('href', data.panelLink);
+    }
+
+    if (data.type === 'main') {
+        let hostCard = rootElementCard.querySelector('[data-info="host"]');
+        let portCard = rootElementCard.querySelector('[data-info="port"]');
+
+        hostCard.after(` ${data.host}`);
+        portCard.after(` ${data.port}`);
+    }
+
+    return cardElement;
+}
+
+function renderingView(data) {
+    data.forEach(element => {
+        const card = fabricCard(element, rootSection);
+
+        if (card) {
+            rootSection.append(card);
+        }
+    });
+}
+
+function initApp() {
+    let state = getInitialState('all_cards', INITIAL_STATE);
+
+    if (!localStorage.getItem('all_cards')) {
+        localStorage.setItem('all_cards', JSON.stringify(state));
+    };
+
+    activeState = state;
+    renderingView(activeState);
+}
+
+initApp();
