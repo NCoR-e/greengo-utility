2cd7beff8b64f954156e6ff4e97fcd539dd787a2
Dmitriy <codingncrploy@gmail.com>
2026-08-31 03:27:14 +0300

    ci: Updating GitHub Actions for dependencies in accordance with GitHub new changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
     root: 'src',
     publicDir: '../public',
-    base: process.env.GITHUB_PAGES ? 'greengo-utility' : '/',
+    base: process.env.GITHUB_PAGES ? '/greengo-utility/' : '/',
 
     resolve: {
         alias: {
